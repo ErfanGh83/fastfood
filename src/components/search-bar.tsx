d0598d6c9f02c3e223dda417eee8b8373455d76b
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-01 19:55:14 +0330

    added some styles to search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -4,25 +4,28 @@ export default function SearchBar(){
 
     const [isExpanded, setIsExpanded] = useState(false);
     const [isSpinning, setIsSpinning] = useState(false);
+    const [searchBarSrc, setSearchBarSrc] = useState('/src/assets/icons/search-bar-icon.png');
 
     const toggleExpand = () => {
 
+        setSearchBarSrc('/src/assets/icons/sharingan-search-icon.webp')
         setIsSpinning(true);
         setTimeout(() => {
         setIsSpinning(false);
-        }, 1000);
+        setSearchBarSrc('/src/assets/icons/search-bar-icon.png')
+        }, 700);
 
         setIsExpanded((prev) => !prev);
     };
     
     return(
-        <div className="search-bar w-380px h-fit rounded-full border-2 border-black bg-slate-200 overflow-hidden flex flex-row">
-            <button className={`w-12 h-12 rounded-full border-slate-400 bg-slate-300 focus:outline-none transition-transform ${
+        <div className="search-bar w-380px h-fit rounded-full border-2 border-black bg-black overflow-hidden flex flex-row">
+            <button className={`w-12 h-12 rounded-full border-slate-400 bg-red-600 focus:outline-none transition-transform ${
           isSpinning ? "animate-spin" : ""
         }`} onClick={toggleExpand}>
-                <img className="w-6 h-6 m-auto" src="src/assets/icons/search-bar-icon.png"/>
+                <img className="w-6 h-6 m-auto" src={searchBarSrc}/>
             </button>
-            <input className={`w-full h-12 rounded-r-full bg-slate-200 text-[18px] transition-all duration-300 ease-in-out focus:outline-none ${
+            <input className={`w-full h-12 rounded-r-full bg-black text-[18px] text-white transition-all duration-300 ease-in-out focus:outline-none ${
           isExpanded ? "w-[200px] ml-2" : "w-0"
         }`} placeholder="search"/>
         </div>
