ae070ae99b0fe289a1e5469397af27bd401f8824
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-01 17:18:36 +0330

    added an example header component

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header(){
+
+    return (
+        <div className="w-full h-[80px] flex flex-row shadow-md justify-between">
+            <h1>HELLO REACT</h1>
+        </div>
+    )
+}
