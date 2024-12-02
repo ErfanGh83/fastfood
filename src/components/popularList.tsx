a64590f850248d8bd0392c3926b31e754b35cc58
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-02 18:35:47 +0330

    added cart that stores foods

--- src/components/popularList.tsx
+++ src/components/popularList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Food = {
     id: number,
     title: string,
@@ -5,13 +7,15 @@ type Food = {
     description: string,
     img_src: string,
     theme: string,
-    tags: string[]
+    tags: string[],
+    count?: number
 }
-
 
 export default function PopularList() {
 
-    const foods : Food[] = [
+    const [cart, setCart] = useState<Food[]>([]);
+
+   const foods : Food[] = [
         {
             id: 1,
             title: 'Naruto Ramen',
@@ -68,38 +72,87 @@ export default function PopularList() {
         },
     ]
 
+    function initializeCart() {
+        return foods.map((food) => ({
+            ...food,
+            count: 0
+        }));
+    }
+
+    if (cart.length === 0) {
+        setCart(initializeCart());
+    }
+
+    function addFood(food: Food) {
+        setCart((prevCart) => {
+            const existingFood = prevCart.find(item => item.id === food.id);
+            if (existingFood) {
+                return prevCart.map(item => 
+                    item.id === food.id ? { ...item, count: item.count + 1 } : item
+                );
+            }
+            return [...prevCart, { ...food, count: 1 }];
+        });
+    }
+
+    function reduceFood(food: Food) {
+
+        setCart((prevCart) => {
+            const updatedCart = prevCart.map(item =>
+                item.id === food.id && item.count? { ...item, count: item.count - 1 } : item
+            );
+
+            return updatedCart;
+        });
+    }
+
     const popularFoods = foods.filter((food)=>food.tags.includes('popular'))
+
     return(
-    <ul className="popular-foods flex flex-row overflow-x-scroll custom-scrollbar w-full my-12 mx-auto snap-x">
-                {popularFoods.map((food)=>(
-                    <li key={food.id} className={`popular-food snap-center min-w-[400px] mx-2 food rounded-3xl overflow-hidden shadow-2xl pb-6 my-6`}>
-                    <img className="w-full h-[300px]" src={food.img_src}></img>
-                    <div className="food-info mx-6 max-h-[400px] min-h-[300px]">
-                        <div className="w-full top-row flex flex-row justify-between">
-                            <div className="food-title flex flex-row">
-                                <img className="w-6 h-6" src="/src/assets/icons/leaf-icon.png"/>
-                                <h1 className="food-name text-[36px] font-semibold">{food.title}</h1> 
-                            </div>
-                            
-                            <button className="shadow-md">
-                                <img className="w-6 h-6" src="/src/assets/icons/3dots-icon.png"/>
-                            </button>
+        <ul className="popular-foods flex flex-row overflow-x-scroll custom-scrollbar w-full my-12 mx-auto snap-x">
+            {popularFoods.map((food)=>(
+                <li key={food.id} className={`popular-food snap-center min-w-[400px] mx-2 food rounded-3xl overflow-hidden shadow-2xl pb-6 my-6`}>
+                <img className="w-full h-[300px]" src={food.img_src}></img>
+                <div className="food-info mx-6 max-h-[400px] min-h-[300px]">
+                    <div className="w-full top-row flex flex-row justify-between">
+                        <div className="food-title flex flex-row">
+                            <img className="w-6 h-6" src="/src/assets/icons/leaf-icon.png"/>
+                            <h1 className="food-name text-[36px] font-semibold">{food.title}</h1> 
                         </div>
                         
-                        <div className="description my-4 h-[200px]">
-                            <p>{food.description}</p>
-                        </div>
-                    </div>
-                    <div className="food-footer flex flex-row justify-between">
-                        <button className={`more-button w-[100px] h-[30px] rounded-3xl shadow-md text-center`} >
-                            <p>Buy</p>
+                        <button className="shadow-md">
+                            <img className="w-6 h-6" src="/src/assets/icons/3dots-icon.png"/>
                         </button>
-                        <h3 className="text-[36px] font-bold mx-2">
-                            ${food.price}
-                        </h3>
                     </div>
-                </li>
-                ))}
-            </ul>
+                    
+                    <div className="description my-4 h-[200px]">
+                        <p>{food.description}</p>
+                    </div>
+                </div>
+                <div className="food-footer flex flex-row justify-between items-center">
+                    <div className={`button-container flex flex-row w-[280px] h-[60px] items-center justify-evenly m-4 bg-gray-100 rounded-3xl shadow-md text-center`} >
+                        <button onClick={()=>{reduceFood(food)}}>
+                            <p className="text-2xl">
+                                -
+                            </p>
+                        </button>
+
+                        <div>
+                            {cart.map((cartFood) => cartFood.id === food.id ? <p key={cartFood.id}>{cartFood.count}</p> : null)}
+                        </div>
+
+                        <button onClick={()=>{addFood(food)}}>
+                            <p className="text-2xl">
+                                +
+                            </p>
+                        </button>
+                    </div>
+                    <h3 className="text-[36px] font-bold mx-2">
+                        ${food.price}
+                    </h3>
+                </div>
+            </li>
+            ))}
+        </ul>
     )
 }
