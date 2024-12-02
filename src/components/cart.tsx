1af0191b2e19129c184008f42f1fc058688242fb
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-02 21:04:29 +0330

    improved style for cart

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -12,12 +12,12 @@ export default function Cart(){
             <div>
                 <ul className='foods w-full flex flex-col h-fit max-h-[400px] overflow-y-auto'>
                     {inCartFoods.map((food)=>(
-                        <li className='food justify-betwee justify-start flex flex-row-reverse items-center border-2 border-gray-200'>
-                            <div className='w-1/4'>
-                                <h1>{food.title}</h1>
+                        <li className='food justify-betwee justify-start flex flex-row-reverse items-center border-2 border-gray-200 justify-between'>
+                            <div className='w-1/3'>
+                                <h1 className='w-1/3 text-center'>{food.title}</h1>
                             </div>
 
-                            <div className={`button-container flex flex-row w-1/4 h-[50px] items-center justify-evenly m-4 bg-gray-100 rounded-3xl shadow-md text-center`} >
+                            <div className={`button-container flex flex-row w-[300px] h-[50px] items-center justify-evenly m-4 bg-gray-100 rounded-3xl shadow-md text-center`} >
                                 <button onClick={()=>{reduceFood(food)}}>
                                     <p className="text-2xl">
                                         -
@@ -35,12 +35,16 @@ export default function Cart(){
                                 </button>
                             </div>
 
-                            <div className='w-1/4'>
-                                <h1>${food.price}</h1>
+                            <div className='w-1/3 text-center'>
+                                <h1>${food.price*food.count}</h1>
                             </div>
                         </li>
                     ))}
                 </ul>
+
+                <div>
+                    <h1 className='text-[48px] font-bold'>Total : {inCartFoods.reduce((a, b) => a + b.count*b.price, 0)}</h1>
+                </div>
             </div>
         </div>
     )
