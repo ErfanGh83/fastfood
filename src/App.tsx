355c72571012f39b8c8f9b3909cf1b6c99375207
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-03 13:53:44 +0330

    added icons to show/hide cart button

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ function App() {
         <Header/>
         <Container/>
         <div className={`w-full bottom-0 left-0 h-fit fixed mt-12`}>
-          <button className='w-[200px] rounded-t-2xl mx-auto text-[36px] border-t-2 border-x-2 border-x-black border-t-black text-center font-semibold bg-yellow-400' onClick={toggleCart}>{isExpanded? "Hide Cart" : "Show Cart"}</button>
-          <div className={`${
-          isExpanded? "block" : "hidden"  }`}>
+          <button className='w-[240px] rounded-t-2xl font-mono mx-auto text-[32px] border-t-2 border-x-2 border-x-black border-t-black text-center font-semibold bg-yellow-400' onClick={toggleCart}>{isExpanded? <div className='flex felx-row items-center '><img className='w-8 h-6 m-auto' src='/src/assets/icons/kunai-down-icon.png'/><p className='mr-6'>Hide Cart</p></div> : <div className='flex felx-row'><img className='w-6 h-6 m-auto' src='/src/assets/icons/kunai-up-icon.png'/><p className='mr-6'>Show Cart</p></div>}</button>
+          <div className={`h-fit transition-all duration-300 ease-in-out ${
+          isExpanded? "max-h-[1000px]" : "max-h-[0px]"  }`}>
             <Cart/>
           </div>
         </div>
