b0bc7c801b284240f087336bd8799d82513da98b
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-03 12:24:57 +0330

    added foods to food list up to 20 different foods

--- src/components/FoodContext.tsx
+++ src/components/FoodContext.tsx
@@ -99,6 +99,114 @@ export const FoodProvider = ({ children }: { children: ReactNode }) => {
             theme: 'brown',
             tags: []
         },
+        {
+            id: 9,
+            title: 'Sukiyaki',
+            price: 24.99,
+            description: `Sukiyaki is a type of hot pot dish known for its sweet and salty flavor, seasoned with shoyu, sugar, and mirin. In addition to thin slices of beef, common ingredients for sukiyaki include naganegi (Japanese leek), shungiku green, shiitake, tofu, and shirataki noodles.`,
+            img_src: '/src/assets/images/sukiyaki-img.jpg',
+            theme: 'gray',
+            tags: []
+        },
+        {
+            id: 10,
+            title: 'Miso soup',
+            price: 7.99,
+            description: `Miso soup is a traditional Japanese soup consisting of miso paste mixed with a dashi stock. It is commonly served as part of an ichijū-sansai meal, meaning "one soup, three dishes," a traditional Japanese meal structure that includes rice, soup, and side dishes. `,
+            img_src: '/src/assets/images/miso-soup-img.jpg',
+            theme: 'gray',
+            tags: []
+        },
+        {
+            id: 11,
+            title: 'Japanese curry',
+            price: 12.99,
+            description: `Japanese curry is commonly served in three main forms: curry over rice, curry udon, and curry bread. It is one of the most popular dishes in Japan. The very common "curry rice" is most often referred to simply as "curry". Along with the sauce, a wide variety of vegetables and meats are used to make Japanese curry.`,
+            img_src: '/src/assets/images/japanese-curry-img.webp',
+            theme: 'brown',
+            tags: []
+        },
+        {
+            id: 12,
+            title: 'Donburi',
+            price: 16.99,
+            description: `Donburi is a Japanese "rice-bowl dish" consisting of fish, meat, vegetables or other ingredients simmered together and served over rice. Donburi meals are usually served in oversized rice bowls which are also called donburi.`,
+            img_src: '/src/assets/images/donburi-img.webp',
+            theme: 'brown',
+            tags: []
+        },
+        {
+            id: 13,
+            title: 'Japanese hamburger',
+            price: 16.99,
+            description: `Hamburg (ハンバーグ, hanbāgu, Hamburg steak) is a popular dish in Japan. It is made from ground meat with finely chopped onion, egg, and breadcrumbs flavored with various spices, and made into a flat, oval shape about 4 cm thick and 10 to 15 cm in diameter.`,
+            img_src: '/src/assets/images/japanese-hamburger-img.jpg',
+            theme: 'brown',
+            tags: []
+        },
+        {
+            id: 14,
+            title: 'Kani salad',
+            price: 16.99,
+            description: `Kani salad combines shredded crab sticks with a homemade, mayo-based dressing made from Japanese mayonnaise (Kewpie). The dressing is spiced with sriracha, and the salad is tossed with tobiko and panko breadcrumbs added for textural contrast and flavor!`,
+            img_src: '/src/assets/images/kani-salad-img.jpg',
+            theme: 'brown',
+            tags: []
+        },
+        {
+            id: 15,
+            title: 'Teriyaki chicken pizza',
+            price: 9.99,
+            description: `Teriyaki chicken is simply chicken that is coated in teriyaki sauce. The dish comes from the Japanese cooking technique called teriyaki, where meat is grilled or broiled with a soy sauce, mirin, and sugar glaze. Today, teriyaki chicken refers to any chicken with teriyaki sauce — regardless of the cooking method.`,
+            img_src: '/src/assets/images/teriyaki-pizza-img.jpg',
+            theme: 'brown',
+            tags: []
+        },
+        {
+            id: 16,
+            title: 'Coca cola',
+            price: 9.99,
+            description: `The Coca-Cola Company is an American corporation founded in 1892 and today engaged primarily in the manufacture and sale of syrup and concentrate for Coca-Cola, a sweetened carbonated beverage that is a cultural institution in the United States and a global symbol of American tastes.`,
+            img_src: '/src/assets/images/coca-cola-img.jpg',
+            theme: 'black',
+            tags: []
+        },
+        {
+            id: 17,
+            title: 'Furikake fries',
+            price: 2.99,
+            description: `Furikake is a Japanese seasoning blend of toasted sesame seeds, dried seaweed, sugar, salt, and other savory ingredients. When sprinkled over a piping hot plate of crispy fries, it's like a flavor explosion in your mouth that's impossible to resist.`,
+            img_src: '/src/assets/images/furikake-fries-img.jpg',
+            theme: 'black',
+            tags: []
+        },
+        {
+            id: 18,
+            title: 'Japdog',
+            price: 6.99,
+            description: `These Japanese hot dogs, or Japadog, are glazed with sweet and tangy teriyaki sauce, Kewpie mayo, bonito flakes, and nori (seaweed) flakes/ strips. They are an umami bombs in hot dog form, inspired by Japadog (the Vancouver street food chain).`,
+            img_src: '/src/assets/images/japdog-img.jpg',
+            theme: 'red',
+            tags: []
+        },
+        {
+            id: 19,
+            title: 'Tonkatsu sauce',
+            price: 0.99,
+            description: `This tonkatsu sauce is made sweet and savory with ketchup, soy sauce, and mirin and can be served with fried pork cutlets, coconut shrimp, ...`,
+            img_src: '/src/assets/images/tonkatsu-sauce-img.jpg',
+            theme: 'red',
+            tags: []
+        },
+        {
+            id: 20,
+            title: 'Jidori chicken',
+            price: 6.99,
+            description: `This tonkatsu sauce is made sweet and savory with ketchup, soy sauce, and mirin and can be served with fried pork cutlets, coconut shrimp, ...`,
+            img_src: '/src/assets/images/jidori-chicken-img.webp',
+            theme: 'red',
+            tags: []
+        },
     ];
 
     const addFood = (food: Food) => {
