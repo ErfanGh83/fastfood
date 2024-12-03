20bdaaafb142cd98a798615d35e4cd686328321f
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-03 12:37:48 +0330

    fixed precision for prices

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -12,7 +12,7 @@ export default function Cart(){
             <div>
                 <ul className='foods w-full flex flex-col h-fit max-h-[400px] overflow-y-auto'>
                     {inCartFoods.map((food)=>(
-                        <li className='food justify-betwee justify-start flex flex-row-reverse items-center border-2 border-gray-200 justify-between'>
+                        <li className='food justify-betwee flex flex-row-reverse items-center border-2 border-gray-200 justify-between'>
                             <div className='w-1/3'>
                                 <h1 className='w-1/3 text-center text-xl'>{food.title}</h1>
                             </div>
@@ -36,15 +36,16 @@ export default function Cart(){
                             </div>
 
                             <div className='w-1/3 text-center'>
-                                <h1 className='text-xl'>${food.price*food.count}</h1>
+                                <h1 className='text-xl'>${(food.price*food.count).toPrecision(4)}</h1>
                             </div>
                         </li>
                     ))}
                 </ul>
                     
                 <form className='flex flex-row justify-between items-center'>
-                    <h1 className='text-[48px] font-bold'>Total : ${inCartFoods.reduce((a, b) => a + b.count*b.price, 0)}</h1>
-                    <button className='bg-red-600 rounded-l-3xl w-[200px] h-[60px] border-2 border-black hover:scale-105'>
+                    <h1 className='text-[48px] font-bold'>Total : ${inCartFoods.reduce((a, b) => a + b.count*b.price, 0).toPrecision(4)}</h1>
+                    <button className='bg-yellow-600 rounded-l-3xl flex flex-row items-center pr-4 justify-evenly w-[200px] h-[60px] border-2 border-black hover:scale-105'>
+                        <img className='w-8 h-8 hover:animate-spin' src='/src/assets/icons/scrollbar-icon.png' />
                         <p className='order-button text-3xl text-center font-semibold hover:text-white'>Order</p>
                     </button>
                 </form>
